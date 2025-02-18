5c27e613ef65c3cd9bd068b081533d04242ced65
Codrut Predoi <codrutpredoi@Codruts-MacBook-Pro-4.local>
2025-02-18 14:57:17 +0200

    fix container 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,5 @@
 set -x
 
 COMMAND=$1
-if [[ -z "${INIT_COMMAND}" ]]; then
-  echo "No Init command was procided"
-else
-  ${INIT_COMMAND}
-fi
 
-if [[ -z "${6}" ]]; then
-  echo "No settings file was provided"
-  mvn $1 
-else
-  SETTINGS=$6
-  mvn $1 
-fi
+mvn $1 
